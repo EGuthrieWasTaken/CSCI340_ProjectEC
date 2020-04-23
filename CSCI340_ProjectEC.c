e66999bef6b1efa84ec45b0f36487d98950b59ca
Ethan Guthrie <guthrieec@cofc.edu>
2020-04-23 18:03:30 -0400

    Updates

--- CSCI340_ProjectEC.c
+++ CSCI340_ProjectEC.c
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 int runCommand(char *input, char *dir, char *path);
@@ -31,18 +32,22 @@ int main(int argc, char const *argv[]){
             getline(&input, &input_size, stdin);
             strtok(input, "\n");
             // Running the command.
-            runCommand(input, dir, path);
+            if(runCommand(input, dir, path) == -1){
+                char error_message[30] = "An error has occurred\n";
+                write(STDERR_FILENO, error_message, strlen(error_message));
+            }
             printf("%s\n", path);
         }
         exit(0);
     }else if(argc == 2){
         // Running program in batch mode.
 
+        exit(0);
     }else{
         // Invalid number of arguments.
         printf("Usage:\t./CSCI340_ProjectEC.out [batch_file]\n");
+        exit(0);
     }
-    return 0;
 }
 
 int runCommand(char *input, char *dir, char *path){
@@ -54,26 +59,30 @@ int runCommand(char *input, char *dir, char *path){
     token = strtok(input, delim);
 
     // Checking if command is built-in or in path.
-    if(strcmp(token, "cd") == 0){
+    if(strcmp(token, "exit") == 0){
+        // Exit command specified.
+        token = strtok(NULL, delim);
+        if(token != NULL){
+            return -1;
+        }
+        return 0;
+    }else if(strcmp(token, "cd") == 0){
         // Changing directory (built-in).
         char *token2;
         // Getting directory.
         token = strtok(NULL, delim);
         if(token == NULL){
-            printf("Invalid arguments for cd!\n");
             return -1;
         }
 
         // Checking for extraneous arguments.
         token2 = strtok(NULL, delim);
         if(token2 != NULL){
-            printf("Invalid arguments for cd!\n");
             return -1;
         }
 
         // Changing directory.
         if(chdir(token) == -1){
-            printf("Invalid directory!\n");
             return -1;
         }
 
@@ -89,7 +98,6 @@ int runCommand(char *input, char *dir, char *path){
         while(token != NULL){
             path_dir = opendir(token);
             if(!path_dir){
-                printf("Directory %s does not exist!\n", token);
                 return -1;
             }
             sprintf(buffer, "%s%s;", buffer, token);
@@ -100,8 +108,15 @@ int runCommand(char *input, char *dir, char *path){
         sprintf(path, "%s", buffer);
         return 0;
     }else{
-        // Running command from path.
-        
+        // Running command from path or current directory.
+        // Checking to see if command is relative/absolute executable.
+        if(!access(token, X_OK)){
+            // Running relative/absolute executable.
+
+        }else{
+            // Running command from path.
+
+        }
         return 0;
     }
 }
