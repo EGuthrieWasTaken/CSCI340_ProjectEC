2bc9c80cfa8e60b329824a226dd27bfaaf208230
Ethan Guthrie <guthrieec@cofc.edu>
2020-04-24 16:06:36 -0400

    Final commit (hopefully)

--- CSCI340_ProjectEC.c
+++ CSCI340_ProjectEC.c
@@ -22,13 +22,12 @@ int main(int argc, char const *argv[]){
     char path[256];
     getcwd(dir, sizeof(dir));
     sprintf(path, "/bin;");
+    int out;
 
     // Checking command-line arguments.
     if(argc == 1){
         // Running program in interactive mode.
-        int out;
         while(strcmp(input, "exit") != 0){
-            //input = "";
             // Printing prompt
             printf("ccsh> ");
             // Getting input and stripping newline.
@@ -37,24 +36,49 @@ int main(int argc, char const *argv[]){
             // Checking if parallel command.
             if(strstr(input, "&") != NULL){
                 // Running parallel commands.
-                runParalellCommands(input, dir, path);
+                out = runParalellCommands(input, dir, path);
+                if(out == 1){
+                    exit(0);
+                }
             }else{
                 // Running single command.
-                pid_t pid = fork();
-                if(pid == 0){
-                    out = runCommand(input, dir, path);
+                out = runCommand(input, dir, path);
+                if(out == 1){
                     exit(0);
-                }else{
-                    waitpid(pid, 0, 0);
-                    if(out == 1){
-                        exit(0);
-                    }
                 }
             }
         }
-        exit(0);
+        printf("howdy\n");
     }else if(argc == 2){
-        // Running program in batch mode.
+        FILE *fp;
+        char *line;
+        size_t len = 0;
+        ssize_t read;
+        // Opening batch file.
+        fp = fopen(argv[1], "r");
+        if(fp == NULL){
+            char error_message[30] = "An error has occurred\n";
+            write(STDERR_FILENO, error_message, strlen(error_message));
+            exit(-1);
+        }
+        // Reading file.
+        while((read = getline(&line, &len, fp)) != -1){
+            strtok(line, "\n");
+            // Checking if parallel command.
+            if(strstr(line, "&") != NULL){
+                // Running parallel commands.
+                out = runParalellCommands(line, dir, path);
+                if(out == 1){
+                    exit(0);
+                }
+            }else{
+                // Running single command.
+                out = runCommand(line, dir, path);
+                if(out == 1){
+                    exit(0);
+                }
+            }
+        }
 
         exit(0);
     }else{
@@ -131,83 +155,90 @@ int runCommand(char *input, char *dir, char *path){
         sprintf(path, "%s", buffer);
         return 0;
     }else{
-        // Running command from path or relative path.
-        if(access(command, X_OK) == 0){
-            // Running command from relative path.
-            // Getting args.
-            char buffer[256];
-            token = strtok(NULL, delim);
-            int counter = 0;
-            if(token != NULL){
-                counter++;
-                sprintf(buffer, "%s", token);
+        pid_t pid = fork();
+        if(pid == 0){
+            // Running command from path or relative path.
+            if(access(command, X_OK) == 0){
+                // Running command from relative path.
+                // Getting args.
+                char buffer[256];
                 token = strtok(NULL, delim);
-            }
-            while(token != NULL){
-                counter++;
-                sprintf(buffer, "%s %s", buffer, token);
-                token = strtok(NULL, delim);
-            }
-
-            char *args[counter + 2];
-            args[0] = command;
-            token = strtok(buffer, delim);
-            for(int i = 1; i < counter + 2; i++){
-                args[i] = token;
-                token = strtok(NULL, delim);
-            }
-            args[counter + 1] = NULL;
-
-            execv(args[0], args);
-            return 0;
-        }else{
-            // Running command from path.
-            const char path_delim[] = ";";
-            char buffer[256];
-            // Searching for command in path.
-            token = strtok(path, path_delim);
-            while(token != NULL){
-                sprintf(buffer, "%s/%s", token, command);
-                if(access(buffer, X_OK) == 0){
-                    // Command found. Executing.
-                    sprintf(command, "%s", buffer);
-                    token = strtok(inputSaved, delim);
+                int counter = 0;
+                if(token != NULL){
+                    counter++;
+                    sprintf(buffer, "%s", token);
                     token = strtok(NULL, delim);
-                    int counter = 0;
-                    if(token != NULL){
-                        counter++;
-                        sprintf(buffer, "%s", token);
-                        token = strtok(NULL, delim);
-                    }
-                    while(token != NULL){
-                        counter++;
-                        sprintf(buffer, "%s %s", buffer, token);
-                        token = strtok(NULL, delim);
-                    }
-
-                    char *args[counter + 2];
-                    args[0] = command;
-                    token = strtok(buffer, delim);
-                    for(int i = 1; i < counter + 2; i++){
-                        args[i] = token;
-                        token = strtok(NULL, delim);
-                    }
-                    args[counter + 1] = NULL;
-
-                    execv(args[0], args);
-                    return 0;
                 }
-                token = strtok(NULL, path_delim);
+                while(token != NULL){
+                    counter++;
+                    sprintf(buffer, "%s %s", buffer, token);
+                    token = strtok(NULL, delim);
+                }
+
+                char *args[counter + 2];
+                args[0] = command;
+                token = strtok(buffer, delim);
+                for(int i = 1; i < counter + 2; i++){
+                    args[i] = token;
+                    token = strtok(NULL, delim);
+                }
+                args[counter + 1] = NULL;
+
+                execv(args[0], args);
+                return 0;
+            }else{
+                // Running command from path.
+                const char path_delim[] = ";";
+                char buffer[256];
+                // Searching for command in path.
+                token = strtok(path, path_delim);
+                while(token != NULL){
+                    sprintf(buffer, "%s/%s", token, command);
+                    if(access(buffer, X_OK) == 0){
+                        // Command found. Executing.
+                        sprintf(command, "%s", buffer);
+                        token = strtok(inputSaved, delim);
+                        token = strtok(NULL, delim);
+                        int counter = 0;
+                        if(token != NULL){
+                            counter++;
+                            sprintf(buffer, "%s", token);
+                            token = strtok(NULL, delim);
+                        }
+                        while(token != NULL){
+                            counter++;
+                            sprintf(buffer, "%s %s", buffer, token);
+                            token = strtok(NULL, delim);
+                        }
+
+                        char *args[counter + 2];
+                        args[0] = command;
+                        token = strtok(buffer, delim);
+                        for(int i = 1; i < counter + 2; i++){
+                            args[i] = token;
+                            token = strtok(NULL, delim);
+                        }
+                        args[counter + 1] = NULL;
+
+                        execv(args[0], args);
+                        return 0;
+                    }
+                    token = strtok(NULL, path_delim);
+                }
+                char error_message[30] = "An error has occurred\n";
+                write(STDERR_FILENO, error_message, strlen(error_message));
             }
-            char error_message[30] = "An error has occurred\n";
-            write(STDERR_FILENO, error_message, strlen(error_message));
+        }else{
+            waitpid(pid, 0, 0);
         }
     }
 }
 
 int runParalellCommands(char *input, char *dir, char *path){
     // Setting default variables.
+    int exit_code = 0;
     char inputSaved[256];
+    char buffer[256];
     sprintf(inputSaved, "%s", input);
     const char delim[] = "&";
     char *token;
@@ -223,30 +254,21 @@ int runParalellCommands(char *input, char *dir, char *path){
     // Putting jobs into array.
     char *args[counter];
     token = strtok(inputSaved, delim);
-    args[0] = token;
-    token = strtok(NULL, delim);
-    for(int i = 1; i < counter; i++){
+    for(int i = 0; i < counter; i++){
         // Trimming leading space.
-        for(int j = 0; j < sizeof(token); j++){
-            token[j] = token[j + 1];
-        }
         args[i] = token;
         token = strtok(NULL, delim);
-    }
-
-    for(int i = 0; i < counter; i++){
-        printf("%s\n", args[i]);
     }
     
     // Running jobs.
     pid_t pids[counter];
-    for(int i = 0; i < sizeof(pids); i++){
+    int out;
+    for(int i = 0; i < counter; i++){
         if((pids[i] = fork()) < 0){
             perror("fork");
             abort();
         }else if(pids[i] == 0){
-            runCommand(args[i], dir, path);
-            exit(0);
+            exit(runCommand(args[i], dir, path));
         }
     }
 
@@ -255,8 +277,11 @@ int runParalellCommands(char *input, char *dir, char *path){
     pid_t pid;
     while(counter > 0){
         pid = wait(&status);
-        counter--;
+        if(status == 256){
+            exit_code = 1;
+        }
+        --counter;
     }
 
-    return 0;
+    return exit_code;
 }
